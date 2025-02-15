0945f8c0fbb71f6be3d145a6d884d30a921248d8
nikoloz <nikusha.tskhvaro@gmail.com>
2025-02-16 02:51:59 +0400

    amaizing

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-[100vh] text-white">
-      <h1 className="text-6xl font-bold mb-4">Oops! Page Not Found</h1>
-      <p className="text-lg mb-6">The page you're looking for doesn't exist.</p>
+    <div className="flex flex-col items-center justify-center h-[100vh] mt-[-85px] text-white bg-center max-xl:mt-[-120px]" style={{backgroundImage: "url('/404-Space .png')"}}>
+      <h1 className="text-6xl font-bold mt-[400px] mb-[20px] max-md:text-[20px] max-md:tracking-[5px]">Oops! Page Not Found</h1>
+      <p className="text-lg mb-6 max-md:text-[15px]">The page you're looking for doesn't exist.</p>
       <Link to="/" className="bg-[#419EBB] py-2 px-6 rounded-md text-lg font-semibold">
         Go Back to Home
       </Link>
